16697af435b7f80f4fdb325a6c9cdb5a95985a23
BOBBY <yunchen.li@my.jcu.edu.au>
2025-08-06 14:45:58 +0800

    Adding class properties and constructors

--- moviecollection.py
+++ moviecollection.py
@@ -8,3 +8,9 @@ from movie import Movie
 
 class MovieCollection:
     """Represents a collection of movies, allowing for loading, adding, sorting, and saving movies."""
+
+    YEAR_WIDTH = 4
+
+    def __init__(self):
+        """Initializes a empty MovieCollection."""
+        self.movies = []
