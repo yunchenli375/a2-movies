26c050f869f5ae6b27ec599704028bf12b8bc6cd
BOBBY <yunchen.li@my.jcu.edu.au>
2025-08-06 12:20:50 +0800

    Test initial values for movies

--- test_movie.py
+++ test_movie.py
@@ -15,4 +15,13 @@ def run_tests():
     assert default_movie.year == 0
     assert default_movie.is_watched is False
 
+    # Test initial-value movie
+    initial_movie = Movie("Thor: Ragnarok", 2017, "Comedy", True)
+    # TODO: Write tests to show this initialisation works
+    print("Test initial-value movie:")
+    print(initial_movie)
+    assert initial_movie.title == "Thor: Ragnarok"
+    assert initial_movie.year == 2017
+    assert initial_movie.category == "Comedy"
+    assert initial_movie.is_watched is True
 
