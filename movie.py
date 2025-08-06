7ed5ba51c8a2a6429c7b9729ad5ec2aaef51ee20
BOBBY <yunchen.li@my.jcu.edu.au>
2025-08-06 10:14:53 +0800

    Add constructor

--- movie.py
+++ movie.py
@@ -4,3 +4,10 @@
 class Movie:
     """Represents a movie with title, year, category, and watched status"""
 
+    def __init__(self, title="", year=0, category="", is_watched=False):
+        """Initializes a Movie instance with title, year, category, and watched status."""
+        self.title = title
+        self.year = year
+        self.category = category
+        self.is_watched = is_watched
+
