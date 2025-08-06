4fcde95e4dbfc38484824805a8ba12ddd1951b9d
BOBBY <yunchen.li@my.jcu.edu.au>
2025-08-06 12:20:40 +0800

    Test default values

--- test_movie.py
+++ test_movie.py
@@ -6,4 +6,13 @@ from movie import Movie
 def run_tests():
     """Test Movie class."""
 
+    # Test empty movie (defaults)
+    print("Test empty movie:")
+    default_movie = Movie()
+    print(default_movie)
+    assert default_movie.title == ""
+    assert default_movie.category == ""
+    assert default_movie.year == 0
+    assert default_movie.is_watched is False
+
 
