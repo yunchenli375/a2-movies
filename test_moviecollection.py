a59fddd1e0cb4911e92e12392b4dab522d6cadfe
BOBBY <yunchen.li@my.jcu.edu.au>
2025-08-06 15:03:29 +0800

    Import and test function definitions

---
+++ test_moviecollection.py
@@ -0,0 +1,9 @@
+"""(Incomplete) Tests for MovieCollection class."""
+
+from movie import Movie
+from moviecollection import MovieCollection
+
+
+def run_tests():
+    """Test MovieCollection class."""
+
