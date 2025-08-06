a4fc5aad13b273a64daeb828ec51a318a98d1084
BOBBY <yunchen.li@my.jcu.edu.au>
2025-08-06 15:03:46 +0800

    Test loading movies

--- test_moviecollection.py
+++ test_moviecollection.py
@@ -13,4 +13,11 @@ def run_tests():
     print(movie_collection)
     assert movie_collection.movies == []
 
+    # Test loading movies
+    print("Test loading movies:")
+    movie_collection.load_movies("movies.json")
+    print(movie_collection)
+    assert (
+        movie_collection.movies
+    )  # Assuming file is non-empty; non-empty list is considered True
 
