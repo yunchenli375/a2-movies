7519dccf30ac3474b21a6234bd0df4f5d3f9f263
BOBBY <yunchen.li@my.jcu.edu.au>
2025-08-06 14:46:12 +0800

    Adding statistics on the number of movies not watched

--- moviecollection.py
+++ moviecollection.py
@@ -18,3 +18,13 @@ class MovieCollection:
     def get_number_of_watched_movies(self):
         """Returns the number of unwatched movies in the collection."""
         return sum(1 for movie in self.movies if movie.is_watched)
+
+    def get_number_of_unwatched_movies(self):
+        """Returns the number of unwatched movies in the collection."""
+        return len(self.movies) - self.get_number_of_watched_movies()
+
+    # About load_movies and save_movies:
+    # Including creation of file object in read file or write file function breaks the principle of single responsibility,
+    # because it mixes file handling with data management.
+    # The save/load function should only handle the serialization of movie data with a file-like object, instead of a specific file path.
+    # Because of the immutable tests in test_moviecollection.py, these functions have to accept a filename as their parameter.
