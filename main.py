7d1cb41a05dbd69bc45d30bad6d769f2f0002f86
BOBBY <yunchen.li@my.jcu.edu.au>
2025-08-07 18:38:25 +0800

    File header information

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Name: Yunchen Li
+Date Started:6/8/20025
+Brief Project Description: A Kivy GUI application for managing movie collections.
+GitHub URL:https://github.com/cp1404-students/a2-movies-yunchenli375
+"""
+
+
